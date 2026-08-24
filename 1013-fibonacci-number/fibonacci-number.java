08726a2787c6cfa2f8bfec70eb8ceaf1a6e68cfa
Pratham U <pratham.u05@gmail.com>
2026-08-25 00:28:41 +0530

    Time: 11 ms (9.66%) | Memory: 42.4 MB (16.09%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,13 +1,7 @@
 class Solution {
     public int fib(int n) {
-        int[] res = new int[n+1];
         if(n==0) return 0;
         if(n==1) return 1;
-        res[0] = 0;
-        res[1] = 1;
-        for(int i=2;i<=n;i++){
-            res[i] =  res[i-2] + res[i-1];
-        }
-        return res[n];
+        return fib(n-1) + fib(n-2);
     }
 }
