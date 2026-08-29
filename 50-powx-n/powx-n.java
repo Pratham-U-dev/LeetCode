cd054d35fbd468f2c46ae1b5a72f24a559bf0380
Pratham U <pratham.u05@gmail.com>
2026-08-29 23:47:27 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (47.27%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if(n < 0){
+            x = 1/x;
+            N = -N;
+        }
+        double res = 1.0;
+        double curr = x;
+        while(N>0){
+            if(N%2==1){
+                res *= curr;
+            }
+            curr *= curr;
+            N /=2;
+        }
+        return res;
+    }
+    
+}
