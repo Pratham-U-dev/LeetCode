cd63c1cb0dbbfca5034ab144c03dd49cef771f32
Pratham U <pratham.u05@gmail.com>
2026-09-20 22:41:32 +0530

    Time: 3 ms (8.39%) | Memory: 44.7 MB (6.52%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -1,10 +1,19 @@
 class Solution {
     public int reverseDegree(String s) {
         int sum = 0;
+        HashMap<Character,Integer> map = new HashMap<>();
         for(int i=0;i<s.length();i++){
-            int index = s.charAt(i) - 97 + 1;
-            int revindex = 27 - index;
-            sum += (i+1) * revindex;
+            char ch = s.charAt(i);
+            if(!map.containsKey(ch)){
+                int index = ch - 97 + 1;
+                int revindex = 27 - index;
+                sum += (i+1) * revindex;
+                map.put(ch,revindex);
+            }
+            else{
+                sum+= (i+1) * map.get(ch);
+            }
+            
         }
         return sum;
     }
